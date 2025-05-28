061c4bb7e46eea99099dbca7bfef67021233ed60
Sean-mn <b01080080329@gmail.com>
2025-05-28 16:16:08 +0900

    .fix Program 수정

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,23 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Server.DBContexts;
+using Server.Services;
+using Server.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+if (string.IsNullOrEmpty(connectionString))
+    throw new Exception("DB_CONNECTION 환경 변수 없음.");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
