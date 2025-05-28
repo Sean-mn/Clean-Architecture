15bdcf1bd10011f356db2cb4dd698b2af4c985a2
Sean-mn <b01080080329@gmail.com>
2025-05-28 13:14:56 +0900

    .add User 모델 구현

---
+++ Server/Server/Entities/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Entities.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+}
