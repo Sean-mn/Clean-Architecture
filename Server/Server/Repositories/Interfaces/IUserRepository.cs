5bc51c819ccc886944ea82ff31a5147a25f44abd
Sean-mn <b01080080329@gmail.com>
2025-05-28 23:15:20 +0900

    .add IUserRepository 인터페이스 구현

---
+++ Server/Server/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Server.Entities.Models;
+
+namespace Server.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<User?> GetUserByIdAsync(int uid);
+    Task<int> AddUserAsync(User user);
+    Task<int> UpdateUserAsync(User user);
+    Task<int> DeleteUserAsync(User user);
+}
