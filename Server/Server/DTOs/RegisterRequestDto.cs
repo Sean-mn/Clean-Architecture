8a2b75eb1ba21b85cca5e6b8089638b4193fbc42
Sean-mn <b01080080329@gmail.com>
2025-05-28 19:59:00 +0900

    .fix Dtos null 처리

--- Server/Server/DTOs/RegisterRequestDto.cs
+++ Server/Server/DTOs/RegisterRequestDto.cs
@@ -2,6 +2,6 @@
 
 public class RegisterRequestDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
 }
