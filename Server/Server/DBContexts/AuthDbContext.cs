ed5b145d2cc9233253a0febb6a1a4cfda277c17e
Sean-mn <b01080080329@gmail.com>
2025-05-28 13:47:54 +0900

    .add AuthDbContext 구현
    - Auth 관련 DbContex 구현

---
+++ Server/Server/DBContexts/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Entities.Models;
+
+namespace Server.DBContexts;
+
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
