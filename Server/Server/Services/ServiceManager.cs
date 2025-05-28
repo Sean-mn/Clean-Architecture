9ad1ba2ddbc8e6bfe78f79562838de061880ae24
Sean-mn <b01080080329@gmail.com>
2025-05-28 14:09:26 +0900

    .add Service 관리 매니저 구현

---
+++ Server/Server/Services/ServiceManager.cs
@@ -0,0 +1,16 @@
+﻿using Server.DBContexts;
+
+namespace Server.Services;
+
+public class ServiceManager
+{
+    private readonly AuthDbContext _context;
+    private AuthService? _authService;
+    
+    public ServiceManager(AuthDbContext context)
+    {
+        _context = context;
+    }
+    
+    public AuthService AuthService => _authService ??= new AuthService(_context);
+}
