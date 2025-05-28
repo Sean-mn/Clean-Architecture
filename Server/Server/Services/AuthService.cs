fd7e8528ba5243af81837df8b14282c6a7790e51
Sean-mn <b01080080329@gmail.com>
2025-05-28 13:48:32 +0900

    .add AuthService 구현
    - 회원가입 기능 구현

---
+++ Server/Server/Services/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.DBContexts;
+using Server.DTOs;
+using Server.Entities.Models;
+
+namespace Server.Services;
+
+public class AuthService : Service
+{
+    public AuthService(AuthDbContext context) : base(context)
+    {
+    }
+
+    public async Task<User> RegisterAsync(RegisterRequestDto dto)
+    {
+        var exists = await _context.Users
+            .AnyAsync(u => u.Username == dto.Username);
+
+        if (exists)
+        {
+            throw new Exception("이미 사용 중인 유저 이름");
+        }
+
+        var newUser = new User
+        {
+            Username = dto.Username,
+            Password = dto.Password
+        };
+        
+        _context.Users.Add(newUser);
+        await _context.SaveChangesAsync();
+
+        return newUser;
+    }
+}
